4c848ebaf94acd68cdb31b2660910804a769b53a
abhishekdharamkar <abhishekdharamkar1@gmail.com>
2022-08-19 02:43:37 +0530

    implement redux addto cart

--- bigbasket-frontend/src/Redux/Reducer/RootReducer.js
+++ bigbasket-frontend/src/Redux/Reducer/RootReducer.js
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux";
-import { gettingProductData, changeQuant, filtered_products_data } from "./EventReducer";
+import { gettingProductData, changeQuant, filtered_products_data,updateCart } from "./EventReducer";
 
 const rootReducers = combineReducers({
     gettingProduct: gettingProductData,
     changingQuant: changeQuant,
-    filteringProduct: filtered_products_data
+    filteringProduct: filtered_products_data,
+    updatingCart:updateCart
 })
 
 export default rootReducers;
